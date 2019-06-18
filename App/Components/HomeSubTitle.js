1c6bb347741d3b93b952ea7a3bb16ecad0f6944c
Dipper GUO <c-guojian@genomics.cn>
2019-06-18 16:03:53 +0800

    add home screen subtitles

---
+++ App/Components/HomeSubTitle.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { View, Text } from "native-base";
+
+export default function HomeSubTitle(props) {
+  const color = props.color || "white";
+  const more = props.more || false;
+  const title = props.title || "Sub Title";
+  const styles = StyleSheet.create({
+    container: {
+      height: 20,
+      flexDirection: "row",
+      alignItems: "center"
+    },
+    block: {
+      backgroundColor: color,
+      width: 10,
+      height: 20,
+      width: 10,
+      marginRight: 5
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "black"
+    },
+    more: {
+      color: "black",
+      fontSize: 12,
+      marginLeft: "auto"
+    }
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.block} />
+      <Text style={styles.title}>{title.toUpperCase()}</Text>
+      <Text style={styles.more}>{more ? "MORE" : ""}</Text>
+    </View>
+  );
+}
--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -24,6 +24,7 @@ import styles from "./Styles/HomeScreenStyle";
 import BottomFooter from "../Components/BottomFooter.js";
 import ImagesSwiper from "../Components/ImagesSwiper.js";
 import HomeMenu from "../Components/HomeMenu.js";
+import HomeSubTitle from "../Components/HomeSubTitle.js";
 
 class HomeScreen extends Component {
   componentDidMount() {
@@ -32,6 +33,60 @@ class HomeScreen extends Component {
       return true;
     });
   }
+
+  renderSubItems = () => {
+    const subItems = [
+      {
+        name: "products",
+        color: "#f1c40f",
+        title: "selected products",
+        more: false
+      },
+      {
+        name: "markets",
+        color: "#2980b9",
+        title: "industry markets",
+        more: true
+      },
+      {
+        name: "deals",
+        color: "#d35400",
+        title: "weekly deals",
+        more: false
+      },
+      {
+        name: "brand",
+        color: "#8e44ad",
+        title: "brand zone",
+        more: false
+      },
+      {
+        name: "business",
+        color: "#3498db",
+        title: "business insights",
+        more: false
+      },
+      {
+        name: "trade",
+        color: "#e74c3c",
+        title: "trade shows",
+        more: true
+      },
+      {
+        name: "you",
+        color: "#2ecc71",
+        title: "just for you",
+        more: false
+      }
+    ];
+    return subItems.map((o, i) => (
+      <View style={{ marginTop: 20 }} key={i}>
+        <HomeSubTitle color={o.color} title={o.title} more={o.more} />
+        <View style={{ height: 100, backgroundColor: "#fff", marginTop: 8 }} />
+      </View>
+    ));
+  };
+
   render() {
     return (
       <Container style={styles.container}>
@@ -46,10 +101,11 @@ class HomeScreen extends Component {
         {/*   </Item> */}
         {/* </Header> */}
 
-        <ImagesSwiper style={{ padding: 0 }} />
-        <HomeMenu />
-
-        <Content padder />
+        <Content>
+          <ImagesSwiper style={{ padding: 0 }} />
+          <HomeMenu />
+          <View style={{ padding: 5 }}>{this.renderSubItems()}</View>
+        </Content>
 
         <BottomFooter {...this.props} />
       </Container>
