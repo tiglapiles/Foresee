88c773e40d3d8ba3326ab63dc75674865757e1dc
Dipper GUO <c-guojian@genomics.cn>
2019-06-19 11:52:12 +0800

    fix footer jump error

--- App/Components/BottomFooter.js
+++ App/Components/BottomFooter.js
@@ -1,77 +1,49 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import {
-  Container,
-  Header,
-  Content,
-  Footer,
-  FooterTab,
-  Button,
-  Icon,
-  Text,
-  View
-} from "native-base";
+import React from "react";
+import { Footer, FooterTab, Button, Icon, Text } from "native-base";
 import styles from "./Styles/BottomFooterStyle";
 
-export default class BottomFooter extends Component {
-  // Prop type warnings
-  static propTypes = {
-    active: PropTypes.string,
-    footerSwitch: PropTypes.func
-  };
-
-  // Defaults for props
-  static defaultProps = {
-    active: "home"
-  };
-
-  buttonTouch = (type, e) => {
-    e.preventDefault();
-    this.props.footerSwitch(type);
-  };
-
-  buttonsActive = (active, name) => {
+export default function BottomFooter(props) {
+  const buttonsActive = (active, name) => {
     return active === name;
   };
-
-  render() {
-    return (
-      <Footer style={styles.footer}>
-        <FooterTab>
-          <Button
-            onPress={e => this.buttonTouch("home", e)}
-            vertical
-            active={this.buttonsActive(this.props.active, "home")}
-          >
-            <Icon name="ios-home" />
-            <Text style={styles.title}>Home</Text>
-          </Button>
-          <Button
-            onPress={e => this.buttonTouch("feeds", e)}
-            vertical
-            active={this.buttonsActive(this.props.active, "feeds")}
-          >
-            <Icon name="ios-heart" />
-            <Text style={styles.title}>Feeds</Text>
-          </Button>
-          <Button
-            onPress={e => this.buttonTouch("messenger", e)}
-            vertical
-            active={this.buttonsActive(this.props.active, "messenger")}
-          >
-            <Icon active name="ios-chatboxes" />
-            <Text style={styles.title}>Messenger</Text>
-          </Button>
-          <Button
-            onPress={e => this.buttonTouch("myforesee", e)}
-            vertical
-            active={this.buttonsActive(this.props.active, "myforesee")}
-          >
-            <Icon name="ios-person" />
-            <Text style={styles.title}>My Foresee</Text>
-          </Button>
-        </FooterTab>
-      </Footer>
-    );
-  }
+  const navigation = props.navigation;
+  const active = props.navigation.state.routeName || "Home";
+  return (
+    <Footer style={styles.footer}>
+      <FooterTab>
+        <Button
+          onPress={() => navigation.navigate("Home")}
+          vertical
+          active={buttonsActive(active, "Home")}
+        >
+          <Icon name="ios-home" />
+          <Text style={styles.title}>Home</Text>
+        </Button>
+        <Button
+          onPress={() => navigation.navigate("Feeds")}
+          vertical
+          active={buttonsActive(active, "Feeds")}
+        >
+          <Icon name="ios-heart" />
+          <Text style={styles.title}>Feeds</Text>
+        </Button>
+        <Button
+          onPress={() => navigation.navigate("Messenger")}
+          vertical
+          active={buttonsActive(active, "Messenger")}
+        >
+          <Icon active name="ios-chatboxes" />
+          <Text style={styles.title}>Messenger</Text>
+        </Button>
+        <Button
+          onPress={() => navigation.navigate("My Foresee")}
+          vertical
+          active={buttonsActive(active, "My Foresee")}
+        >
+          <Icon name="ios-person" />
+          <Text style={styles.title}>My Foresee</Text>
+        </Button>
+      </FooterTab>
+    </Footer>
+  );
 }
