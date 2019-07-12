fee52915d1a79747e3e88cc339f3a3ae4701610d
Dipper GUO <c-guojian@genomics.cn>
2019-07-12 10:14:41 +0800

    add qr scan camera
    1. add lib react-native-camera and react-native-qrcode-scanner

--- App/Containers/Styles/ScanScreenStyle.js
+++ App/Containers/Styles/ScanScreenStyle.js
@@ -1,6 +1,23 @@
-import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { StyleSheet } from "react-native";
+import { ApplicationStyles } from "../../Themes/";
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
-})
+  ...ApplicationStyles.screen,
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: "#777"
+  },
+  textBold: {
+    fontWeight: "500",
+    color: "#000"
+  },
+  buttonText: {
+    fontSize: 21,
+    color: "rgb(0,122,255)"
+  },
+  buttonTouchable: {
+    padding: 16
+  }
+});
--- android/app/src/main/java/com/foresee/MainApplication.java
+++ android/app/src/main/java/com/foresee/MainApplication.java
@@ -3,6 +3,7 @@ package com.foresee;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new ReactVideoPackage(),
             new ReactNativeConfigPackage(),
             new ReactNativeI18n(),
