e4b06e69266f0db2fa1088dc968f087795538d2a
Dipper GUO <c-guojian@genomics.cn>
2019-07-17 12:14:07 +0800

    modified video topic styles

--- App/Components/FeedsTopic.js
+++ App/Components/FeedsTopic.js
@@ -1,49 +1,34 @@
 import React from "react";
-import { ScrollView, Image, TouchableOpacity } from "react-native";
+import { TouchableOpacity, FlatList, ImageBackground } from "react-native";
 import { View, Text } from "native-base";
 import styles from "./Styles/FeedsTopicStyle";
 
 export default function FeedsTopic(props) {
-  const topics = [
-    {
-      img:
-        "https://sc01.alicdn.com/kf/HTB1bgyOXTjxK1Rjy0Fnq6yBaFXag.jpg_100x100.jpg"
-    },
-    {
-      img:
-        "https://sc01.alicdn.com/kf/HTB1bgyOXTjxK1Rjy0Fnq6yBaFXag.jpg_100x100.jpg"
-    },
-    {
-      img:
-        "https://sc01.alicdn.com/kf/HTB1bgyOXTjxK1Rjy0Fnq6yBaFXag.jpg_100x100.jpg"
-    },
-    {
-      img:
-        "https://sc01.alicdn.com/kf/HTB1bgyOXTjxK1Rjy0Fnq6yBaFXag.jpg_100x100.jpg"
-    },
-    {
-      img:
-        "https://sc01.alicdn.com/kf/HTB1bgyOXTjxK1Rjy0Fnq6yBaFXag.jpg_100x100.jpg"
-    },
-    {
-      img:
-        "https://sc01.alicdn.com/kf/HTB1bgyOXTjxK1Rjy0Fnq6yBaFXag.jpg_100x100.jpg"
-    }
-  ];
-  const renderTopics = d =>
-    d.map((i, k) => (
-      <View key={k} style={styles.item}>
-        <TouchableOpacity onPress={() => props.navigation.navigate("PayLater")}>
-          <Image source={{ uri: i.img }} style={{ width: 80, height: 100 }} />
-        </TouchableOpacity>
-      </View>
-    ));
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Topic</Text>
-      <ScrollView contentContainerStyle={styles.scroll} horizontal>
-        {renderTopics(topics)}
-      </ScrollView>
+      <FlatList
+        ItemSeparatorComponent={({ highlighted }) => (
+          <View style={[highlighted && { marginLeft: 0 }]} />
+        )}
+        data={require("../Fixtures/topic.json")}
+        horizontal={true}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item, index, separator }) => (
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => props.navigation.navigate("PayLater")}
+          >
+            <ImageBackground source={{ uri: item.img }} style={styles.img}>
+              <View style={styles.textView}>
+                <Text style={styles.hash} numberOfLines={2}>
+                  #FashionTrend
+                </Text>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
--- App/Components/Styles/FeedsTopicStyle.js
+++ App/Components/Styles/FeedsTopicStyle.js
@@ -3,18 +3,16 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   container: {
     // flex: 1
-    height: 140,
+    padding: 10,
+    height: 180,
     backgroundColor: "#fff",
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center"
   },
-  scroll: {
-    // flexDirection: "row",
-    // justifyContent: "flex-start",
-    // alignItems: "center",
+  img: {
     width: "100%",
-    paddingLeft: 10
+    height: "100%"
   },
   title: {
     marginLeft: 10,
@@ -24,11 +22,26 @@ export default StyleSheet.create({
     fontSize: 20
   },
   item: {
-    width: 80,
-    height: 100,
+    width: 100,
+    height: 120,
     borderRadius: 5,
     overflow: "hidden",
     marginRight: 8,
     backgroundColor: "#f39c12"
+  },
+  hash: {
+    color: "#fff",
+    fontSize: 12,
+    backgroundColor: "transparent"
+  },
+  textView: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: 8
   }
 });
